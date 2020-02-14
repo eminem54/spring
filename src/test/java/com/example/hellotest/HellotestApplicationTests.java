3c0f23cc41d4d3e8e222f8caafebf242c6e6e51f
EasyZero <eminem54@naver.com>
2020-02-14 20:20:34 +0900

    [ Feat ] 프로젝트 생성

---
+++ src/test/java/com/example/hellotest/HellotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
