09fd8e629a1076ac4428ed1033804c7f49ae36ef
EasyZero <eminem54@naver.com>
2020-02-16 14:09:16 +0900

    [ Feat ]  글 목록 가져오는 view 추가
    부트스트랩으로 mustache 로 게시판 틀 추가

--- src/main/java/com/example/hellotest/service/posts/PostsService.java
+++ src/main/java/com/example/hellotest/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.hellotest.service.posts;
 
 import com.example.hellotest.domain.posts.Posts;
 import com.example.hellotest.domain.posts.PostsRepository;
+import com.example.hellotest.web.dto.PostsListResponseDto;
 import com.example.hellotest.web.dto.PostsResponseDto;
 import com.example.hellotest.web.dto.PostsSaveRequestDto;
 import com.example.hellotest.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/hellotest/web/IndexController.java
+++ src/main/java/com/example/hellotest/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.example.hellotest.web;
 
+import com.example.hellotest.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
