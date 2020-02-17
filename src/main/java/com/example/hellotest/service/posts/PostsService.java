e9cea77e9432132db83c49c8afcd68eda741e802
EasyZero <eminem54@naver.com>
2020-02-17 13:08:38 +0900

    [ Feat ]  oauth 설정 추가

--- src/main/java/com/example/hellotest/service/posts/PostsService.java
+++ src/main/java/com/example/hellotest/service/posts/PostsService.java
@@ -46,4 +46,12 @@ public class PostsService {
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
 }
